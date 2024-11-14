a715a0da4b90f04d5be75fbf377da0ab23f2e5fd
WARlord1903 <williamccs2014@gmail.com>
2024-11-14 13:50:08 -0500

    Added user_agents dependency for browser detection

--- start.ps1
+++ start.ps1
@@ -6,7 +6,7 @@ else{
     Write-Output "Virtual Environment not found, creating and installing dependencies..."
     python -m venv clevercart_env
     .\clevercart_env\Scripts\Activate.ps1
-    python -m pip install flask flask-sqlalchemy flask-login gevent selenium openai
+    python -m pip install flask flask-sqlalchemy flask-login user_agents gevent selenium openai
 }
 
 python main.py
